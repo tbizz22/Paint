f0d8b1589b9e6f89ef0728b1222ea44db468c926
Tyler Bisbee <tyler.bisbee@gmail.com>
2019-11-18 14:02:36 -0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+M.AutoInit();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
